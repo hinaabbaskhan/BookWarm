2439250df01ac6c9304c2f6b3fe750e58dcc6a73
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-07-21 00:24:25 +0500

    initial commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Hina Khan on 21/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
