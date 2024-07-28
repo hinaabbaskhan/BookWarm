b2352ba10d2e0c2709bac9a5464406d868e21f4d
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-07-28 23:45:24 +0500

    add model container

--- Bookworm/BookwormApp.swift
+++ Bookworm/BookwormApp.swift
@@ -13,7 +13,8 @@ struct BookwormApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-        }.modelContainer(for: Student.self)
+        }.modelContainer(for: Student.self).modelContainer(for: Book.self)
+
 
     }
 }
