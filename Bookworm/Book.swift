38a62b4b77bf8cb03cfd4a66b4bd2c23b4d226bb
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-07-28 23:39:53 +0500

    add book model

---
+++ Bookworm/Book.swift
@@ -0,0 +1,18 @@
+//
+//  Book.swift
+//  Bookworm
+//
+//  Created by Hina Khan on 28/07/2024.
+//
+
+import SwiftData
+import Foundation
+
+@Model
+class Book {
+    var title: String
+    var author: String
+    var genre: String
+    var review: String
+    var rating: Int
+}
