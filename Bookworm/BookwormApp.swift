bb93bef4bf48cfacbd275652d13f2a4917fb43ef
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-09-22 16:42:00 +0500

    Update BookwormApp.swift

--- Bookworm/BookwormApp.swift
+++ Bookworm/BookwormApp.swift
@@ -13,7 +13,7 @@ struct BookwormApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-        }.modelContainer(for: Student.self).modelContainer(for: Book.self)
+        }.modelContainer(for: Book.self)
 
 
     }
